2981dfe3319dd9671a2d6a2e24be1c8ee9cf5e46
SK Arbaj Ali <skarbajali.one@outlook.com>
2023-12-09 23:55:31 +0530

    Fix: Style updated for mobile responsiveness and some markup improvement.

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 let anchorLinks = document.querySelectorAll('.link');
+let crossIcon = document.querySelector('img[alt="cross-icon"]');
+let burgerMenuIcon = document.querySelector('img[alt="burger-menu"]');
+let mobileLinks = document.querySelector('.mobile-links');
+let mobileLinksArray = document.querySelectorAll('.mobile-links .link');
 
 anchorLinks.forEach((elem,index) => {
     elem.addEventListener('mouseover', (event)=>{
@@ -8,5 +12,21 @@ anchorLinks.forEach((elem,index) => {
 anchorLinks.forEach((elem,index) => {
     elem.addEventListener('mouseout', (event)=>{
         elem.nextElementSibling.classList.toggle('border-bottom-hover');
+    })
+})
+
+burgerMenuIcon.addEventListener('click', (event)=>{
+    mobileLinks.style.display="flex";
+    // console.log(mobileLinks);
+})
+
+crossIcon.addEventListener('click', (event)=>{
+    mobileLinks.style.display="none";
+    // console.log(mobileLinks.className);
+})
+
+mobileLinksArray.forEach((elem,index)=>{
+    elem.addEventListener('click', (event)=>{
+        mobileLinks.style.display="none";
     })
 })
